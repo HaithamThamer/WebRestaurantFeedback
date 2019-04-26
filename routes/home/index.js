1a208162374977c6e41268ab9be1a847e18b777c
Haitham Alhaji (Laptop) <hiethem.heithem@yahoo.com>
2019-04-26 21:40:02 +0300

    styling

--- routes/home/index.js
+++ routes/home/index.js
@@ -43,9 +43,9 @@ router.get("/rate/:val", (req, res) => {
     });
   }
   mysqlConnection.getConnection((err, connection) => {
-    // console.log(
-    //   `SELECT format(avg(if(VALUE = 1,1,0)),1) AS \`like\`, format(avg(if(VALUE = 0,1,0)),1) AS \`unlike\`, sum(value) as \`amount\` FROM tbl_ratings ratings WHERE ratings.creation >= DATE_SUB('${myDate_string}', INTERVAL 3 HOUR)`
-    // );
+    console.log(
+      `SELECT format(avg(if(VALUE = 1,1,0)),1) AS \`like\`, format(avg(if(VALUE = 0,1,0)),1) AS \`unlike\`, count(value) as \`amount\` FROM tbl_ratings ratings WHERE ratings.creation >= DATE_SUB('${myDate_string}', INTERVAL 3 HOUR)`
+    );
     connection.query(
       `SELECT format(avg(if(VALUE = 1,1,0)),1) AS \`like\`, format(avg(if(VALUE = 0,1,0)),1) AS \`unlike\`, count(value) as \`amount\` FROM tbl_ratings ratings WHERE ratings.creation >= DATE_SUB('${myDate_string}', INTERVAL 3 HOUR)`,
       (errors, results, fields) => {
